62098fbfbd8f9cec2fb6cca58e12bbe08d42e250
Rin <c0a2205288@edu.teu.ac.jp>
2023-06-27 15:27:14 +0900

    練習問題３

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -40,6 +40,23 @@ class Bird:
         引数1 num:こうかとん画像ファイル名の番号
         引数2 xy:こうかとん画像の位置座標タプル
         """
+        img0 = pg.transform.rotozoom(pg.image.load(f"ex03/fig/{num}.png"), 0, 2.0) # 左向き
+        img = pg.transform.flip(img0, True, False) # 右向き
+        self.imgs = {
+            (+5, 0): img, #　右
+            (+5, -5): pg.transform.rotozoom(img, 45, 1.0),
+            (0, -5):pg.transform.rotozoom(img, 90, 1.0), # 右上
+            (-5, -5): pg.transform.rotozoom(img0, -45, 1.0),
+            (-5, 0): img0,
+            (-5, +5): pg.transform.rotozoom(img0, 45, 1.0),
+            (0, +5): pg.transform.rotozoom(img0, -90, 1.0),
+            (+5, +5): pg.transform.rotozoom(img0, -45, 1.0),
+        }
+        self.img = self.imgs[(+5, 0)]
+        self.rct = self.img.get_rect()
+        self.rct.center = xy
+
+
         self.img = pg.transform.flip(  # 左右反転
             pg.transform.rotozoom(  # 2倍に拡大
                 pg.image.load(f"ex03/fig/{num}.png"), 
@@ -74,6 +91,8 @@ class Bird:
         self.rct.move_ip(sum_mv)
         if check_bound(self.rct) != (True, True):
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0): # 何かしらの矢印キーが押されていたら
+            self.img = self.imgs[tuple(sum_mv)]
         screen.blit(self.img, self.rct)
 
 
