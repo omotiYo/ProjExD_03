40cfad9ee77eb29db8523564c9fc2cfc4515476e
Rin <c0a2205288@edu.teu.ac.jp>
2023-06-27 14:44:26 +0900

    練習問題２

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -158,6 +158,8 @@ def main():
             if beam.rct.colliderect(bomb.rct):
                 bomb = None
                 beam = None
+                bird.change_img(6, screen)
+                pg.display.update()
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
